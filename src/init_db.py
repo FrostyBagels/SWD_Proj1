bacf5ae29136fe50e4e17c357951187acebb250e
Cameron Greeson <cgreeson22@gmail.com>
2026-09-22 23:27:07 -0600

    Add courses

--- src/init_db.py
+++ src/init_db.py
@@ -10,7 +10,11 @@ from app.models import Course
 
 # TODO
 courses = [
-    
+    ('CS', '3250', 'Software Development Methods and Tools', 4),
+    ('CS', '2240', 'Discrete Structures', 4),
+    ( 'MTH', '3210', 'Probability and Statistics', 4),
+    ('THE', '2295', 'Comedy: In-Print and On-Stage', 3),
+    ('MTH', '3130', 'Linear Algebra', 4)
 ]
 
 with app.app_context():
